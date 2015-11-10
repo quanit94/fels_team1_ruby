47c7c2d83a5cc5560c73e396385f77c1213d07a4
quanit94 <quanit94@gmail.com>
2015-11-10 19:14:52 +0700

    signup done

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,15 @@
 Rails.application.routes.draw do
   root 'static_pages#home'
-
+  #static page
   get "help" => 'static_pages#help'
   get "about" => 'static_pages#about'
   get "contact" => 'static_pages#contact'
-  
+  #user
+  get 'signup'  => 'users#new'
+  get 'login'   => 'users#login'
+
+  resources :users
+
   namespace "admin" do
     root 'static_pages#home'
     resources :users
