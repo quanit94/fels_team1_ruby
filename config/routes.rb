7544e3e1f68287e59e7fde14b3276c7a8b35b823
quanit94 <quanit94@gmail.com>
2015-11-10 19:14:52 +0700

    signup done

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,16 @@
 Rails.application.routes.draw do
   root 'static_pages#home'
-
+  #static page
   get "help" => 'static_pages#help'
   get "about" => 'static_pages#about'
   get "contact" => 'static_pages#contact'
+
+  #user
+  get 'signup'  => 'users#new'
+  get 'login'   => 'users#login'
+
+  resources :users
+
   namespace "admin" do
     root 'static_pages#home'
   end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151107044559) do
+ActiveRecord::Schema.define(version: 20151107033208) do
 
   create_table "activities", force: :cascade do |t|
     t.integer  "target_id"
@@ -62,9 +62,10 @@ ActiveRecord::Schema.define(version: 20151107044559) do
   create_table "users", force: :cascade do |t|
     t.string   "name"
     t.string   "email"
-    t.datetime "created_at",      null: false
-    t.datetime "updated_at",      null: false
-    t.string   "password_digest"
+    t.string   "password"
+    t.boolean  "admin",      default: false
+    t.datetime "created_at",                 null: false
+    t.datetime "updated_at",                 null: false
   end
 
   create_table "word_answers", force: :cascade do |t|
