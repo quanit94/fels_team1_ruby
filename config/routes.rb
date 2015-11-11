33629acd3c258f3b6f2a6b48bc89ebe6856e3d3b
quanit94 <quanit94@gmail.com>
2015-11-11 13:06:47 +0700

    Login-logout done

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+
   root 'static_pages#home'
   #static page
   get "help" => 'static_pages#help'
@@ -7,7 +9,10 @@ Rails.application.routes.draw do
 
   #user
   get 'signup'  => 'users#new'
-  get 'login'   => 'users#login'
+
+  get    'login'   => 'sessions#new'
+  post   'login'   => 'sessions#create'
+  delete 'logout'  => 'sessions#destroy'
 
   resources :users
 
