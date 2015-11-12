ab350771e8ec0f6e90ddc27a9bf699f8decdacdc
quanit94 <quanit94@gmail.com>
2015-11-12 15:42:39 +0700

    login_logout done

--- app/models/user.rb
+++ app/models/user.rb
@@ -30,6 +30,7 @@ class User < ActiveRecord::Base
 
   # Returns true if the given token matches the digest.
   def authenticated?(remember_token)
+  	return false if remember_digest.nil?
     BCrypt::Password.new(remember_digest).is_password?(remember_token)
   end
 
@@ -37,6 +38,8 @@ class User < ActiveRecord::Base
   def forget
     update_attribute(:remember_digest, nil)
   end
+
+ 
 
 
 end
