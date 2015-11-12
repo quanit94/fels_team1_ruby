a2667c53789618673528cba0f89e28d9077a5142
Hienmv94 <hienmv94@gmail.com>
2015-11-12 13:47:46 +0700

    admin

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   get "help" => 'static_pages#help'
   get "about" => 'static_pages#about'
   get "contact" => 'static_pages#contact'
+<<<<<<< HEAD
   #user
   get 'signup'  => 'users#new'
 
@@ -16,8 +17,13 @@ Rails.application.routes.draw do
   resources :users
 
   namespace "admin" do
+=======
+  
+  namespace :admin do
+>>>>>>> admin
     root 'static_pages#home'
     resources :users
+    resources :categories
   end
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
+<<<<<<< HEAD
 ActiveRecord::Schema.define(version: 20151111041025) do
+=======
+ActiveRecord::Schema.define(version: 20151110122217) do
+>>>>>>> admin
 
   create_table "activities", force: :cascade do |t|
     t.integer  "target_id"
@@ -22,6 +26,12 @@ ActiveRecord::Schema.define(version: 20151111041025) do
   end
 
   add_index "activities", ["user_id"], name: "index_activities_on_user_id"
+
+  create_table "admin_categories", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "categories", force: :cascade do |t|
     t.string   "name"
