47c2e8858f33380ab57c9d0b9a67a696c9b9e9a6
Hienmv94 <hienmv94@gmail.com>
2015-11-14 09:57:55 +0700

    admin CRUD user done

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -69,6 +69,6 @@ class Admin::UsersController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def user_params
-      params.require(:admin_user).permit(:name, :email, :password, :password_confirmation)
+      params.require(:user).permit(:name, :email, :password, :password_confirmation)
     end
 end
